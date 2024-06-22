6005fa046653b55745db0eb7c5520a64fa5848c4
Ans5H <144544716+Ans5H@users.noreply.github.com>
2024-06-22 19:02:46 +0530

    changed some env variables

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import {getFirestore} from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: 'AIzaSyB-ykPMqtBiWo5eUWdbeTAkmUiYghl_dDI',
+  authDomain: "auth-production-2c326.firebaseapp.com",
+  projectId: "auth-production-2c326",
+  storageBucket: "auth-production-2c326.appspot.com",
+  messagingSenderId: "380395640464",
+  appId:  "1:380395640464:web:e909dc63aeaf5bbeb9ea15"
 };
 
 // Initialize Firebase
